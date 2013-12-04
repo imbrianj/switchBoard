28a294ac2debc8e4974b106d498c3b375a1bb7ca
imbrianj <bjohnsonwsu@gmail.com>
2013-12-03 22:12:27 -0800

    More work on Panasonic TVs.  This is probably very close, but completely untested (I dont have a TV to work off of).

--- controllers/panasonicController.js
+++ controllers/panasonicController.js
@@ -8,6 +8,8 @@ exports.panasonicController = exports.panasonicController || (function () {
    * @author brian@bevey.org
    * @fileoverview Basic control of Panasonic Smart TV
    * @requires http
+   * @note This is a full-scale rip-off of the fine work done here:
+   *       http://cocoontech.com/forums/topic/21266-panasonic-viera-plasma-ip-control/page-2
    */
   return {
     version : '0.0.0.0.1 alpha',
@@ -17,44 +19,53 @@ exports.panasonicController = exports.panasonicController || (function () {
     keymap  : ['30S_SKIP', '3D', 'BD', 'BLUE', 'CANCEL', 'CC', 'CHG_INPUT', 'CHG_NETWORK', 'CH_DOWN', 'CH_UP', 'D0', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'DATA', 'DIGA_CTL', 'DISP_MODE', 'DMS_CH_DOWN', 'DMS_CH_UP', 'DOWN', 'DRIVE', 'ECO', 'ENTER', 'EPG', 'EZ_SYNC', 'FAVORITE', 'FF', 'GAME', 'GREEN', 'HOLD', 'INDEX', 'INFO', 'INTERNET', 'LEFT', 'MENU', 'MPX', 'MUTE', 'OFFTIMER', 'PAUSE', 'PICTAI', 'PLAY', 'POWER', 'PROG', 'P_NR', 'REC', 'RECLIST', 'RED', 'RETURN', 'REW', 'RIGHT', 'R_SCREEN', 'R_TUNE', 'R_TUNE', 'SAP', 'SD_CARD', 'SKIP_NEXT', 'SKIP_PREV', 'SPLIT', 'STOP', 'STTL', 'SUBMENU', 'SWAP', 'TEXT', 'TV', 'TV_MUTE_OFF', 'TV_MUTE_ON', 'UP', 'VIERA_LINK', 'VOD', 'VOLDOWN', 'VOLUP', 'VTOOLS', 'YELLOW'],
 
     /**
-     * Prepare XML request.
-     */
-    requestEnvelope : function (config) {
-      var response = '';
-
-      response += '<?xml version="1.0" encoding="utf-8"?>';
-      response += '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">';
-      response += '<s:Body>';
-      response += '  <u:' + config.action + ' xmlns:u="urn:' + config.urn + '">';
-      response += '    ' + config.args;
-      response += '  </u:' + config.action + '>';
-      response += '</s:Body>';
-      response += '</s:Envelope>';
-
-      return response;
-    },
-
-    /**
-     * Prepare a POST request for a Roku command.
+     * Prepare a POST request for a command.
      */
     postPrepare : function (that) {
       var path    = '/',
           method  = 'POST';
 
-      if(this.command) {
-        path += 'keypress/' + this.command;
+      if(that.command || that.text) {
+        path += 'nrc/control_0';
       }
 
-      if(this.text) {
-
+      else {
+        path += 'dmr/control_0';
       }
 
       return {
-        host    : this.deviceIp,
-        port    : this.devicePort,
+        host    : that.deviceIp,
+        port    : that.devicePort,
         path    : path,
         method  : method
       };
+    },
+
+    /**
+     * Prepare a POST data the request.
+     */
+    postData : function (that) {
+      var response = '',
+          action   = 'X_SendKey',
+          urn      = 'panasonic-com:service:p00NetworkControl:1',
+          value    = '<X_KeyEvent>' + that.command + '</X_KeyEvent>';
+
+      if(that.text) {
+        action = 'X_SendString';
+        urn    = 'panasonic-com:service:p00NetworkControl:1';
+        value  = '<X_String>' + that.text + '</X_String>';
+      }
+
+      response += '<?xml version="1.0" encoding="utf-8"?>';
+      response += '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">';
+      response += '<s:Body>';
+      response += '  <u:' + action + ' xmlns:u="urn:' + urn + '">';
+      response += '    ' + value;
+      response += '  </u:' + action + '>';
+      response += '</s:Body>';
+      response += '</s:Envelope>';
+
+      return response;
     },
 
     send : function (config) {
@@ -99,6 +110,7 @@ exports.panasonicController = exports.panasonicController || (function () {
         that.cbError(errorMsg);
       });
 
+      request.write(this.postData(that));
       request.end();
 
       return dataReply;
