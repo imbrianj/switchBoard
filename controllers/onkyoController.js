504b28cf00c7e9e4602b6693720854d208f47567
imbrianj <bjohnsonwsu@gmail.com>
2013-12-17 20:46:26 -0800

    Add basic skeleton for Onkyo controller.  Still looking for some docs.  Add navigation markup

---
+++ controllers/onkyoController.js
@@ -0,0 +1,21 @@
+/*jslint white: true */
+/*global exports, String, Buffer, require, console */
+
+exports.onkyoController = exports.onkyoController || (function () {
+  'use strict';
+
+  /**
+   * @author brian@bevey.org
+   * @fileoverview Basic control of Onkyo receiver
+   */
+  return {
+    version : '0.0.0.0.1 alpha',
+    /**
+     * Whitelist of available key codes to use.
+     */
+    keymap  : [],
+
+    findState : function () {
+    }
+  };
+} ());
---
+++ templates/fragments/navigation.tpl
@@ -0,0 +1,1 @@
+        <li class="{{DEVICE_ID}}"><a href="#{{DEVICE_ID}}">{{DEVICE_TITLE}}</a></li>
