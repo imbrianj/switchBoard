a64716cf8d6893537894fd6c401c61da9082a93d
imbrianj <bjohnsonwsu@gmail.com>
2014-03-23 02:54:51 -0700

    Add files to testRunner.js.  Ill do something smarter here later

--- tests/testRunner.js
+++ tests/testRunner.js
@@ -10,5 +10,11 @@ module.exports = (function () {
   var reporter = require('nodeunit').reporters.default;
 
   reporter.run(['tests/unit/lib/loadControllerTest.js',
-                'tests/unit/lib/loadMarkupTest.js']);
+                'tests/unit/lib/loadMarkupTest.js',
+                'tests/unit/lib/runCommandTest.js',
+                'tests/unit/lib/staticAssetsTest.js',
+                'tests/unit/controllers/ps3ControllerTest.js',
+                'tests/unit/controllers/rokuControllerTest.js',
+                'tests/unit/controllers/samsungControllerTest.js',
+                'tests/unit/controllers/speechControllerTest.js']);
 }());
