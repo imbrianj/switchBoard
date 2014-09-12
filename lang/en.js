f81c8405ef1bc58225e1e04694cb4031a8fd0890
jfeiler87 <asa23b@gmail.com>
2014-09-12 19:33:32 +0200

    Translation replace errors fixed

--- lang/en.js
+++ lang/en.js
@@ -31,7 +31,7 @@
  */
 
 module.exports = (function () {
-  "use strict";
+  'use strict';
 
   return {
     version : 20140903,
@@ -343,8 +343,8 @@ module.exports = (function () {
         welcome : {
           WELCOME   : "Welcome",
           HEADER    : "Welcome!",
-          SUCCESS   : "Welcome to SwitchBoard.  If you"re seeing this message, it means you"ve got everything set up and you"ll just need to configure your remote.",
-          CONFIGURE : "Open up the config/config.js file which contains a series of standard device types.  Find any device that looks like one you"d like configured and plug in your relevant information.  For anything that"s maybe not super obvious, there should be ample comments to help guide you.  While you"re in there, you may also want to mark the \"welcome\" device as disabled: true to prevent this message from showing up anymore.",
+          SUCCESS   : "Welcome to SwitchBoard.  If you're seeing this message, it means you've got everything set up and you'll just need to configure your remote.",
+          CONFIGURE : "Open up the config/config.js file which contains a series of standard device types.  Find any device that looks like one you'd like configured and plug in your relevant information.  For anything that's maybe not super obvious, there should be ample comments to help guide you.  While you're in there, you may also want to mark the \"welcome\" device as disabled: true to prevent this message from showing up anymore.",
           SUPPORT   : "Stuck?  Have questions?  Something go wrong or have any suggestions?  Feel free to <a href=\"https://github.com/imbrianj/switchBoard/issues/new\" rel=\"external\">file a ticket</a> or send me an email at <a href=\"mailto:brian@bevey.org\" rel=\"external\">brian@bevey.org</a>."
         },
         xbmc : {
