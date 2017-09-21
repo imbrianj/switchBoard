d2d98eca2544bc7b56f771e37021b6d0913c7c3c
imbrianj <bjohnsonwsu@gmail.com>
2017-09-20 20:29:11 -0700

    Bump request package. And manifest (nobody uses it yet anyway). Add weekday/weekend and time of day vars to ai conditions. Add simple test.

---
+++ tests/unit/lib/ai.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2014 brian@bevey.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
+ * @author brian@bevey.org
+ * @fileoverview Unit test for ai.js
+ */
+
+exports.ai = {
+  getTimeCategory : function (test) {
+    'use strict';
+
+    var ai          = require(__dirname + '/../../../lib/ai'),
+        workdayMorn = ai.getTimeCategory(1451073600),
+        workdayEve  = ai.getTimeCategory(1451023600);
+
+    test.strictEqual(workdayMorn, 'workday-morning-', 'Find workday morning category name');
+    test.strictEqual(workdayEve,  'workday-evening-', 'Find workday evening category name');
+    test.done();
+  }
+};
