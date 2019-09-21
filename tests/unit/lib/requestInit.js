8d7e94586972d9698e74f67ded3937adc27ad837
imbrianj <bjohnsonwsu@gmail.com>
2019-09-21 12:07:01 -0700

    More defensive check if requestIsStream.  Bump websocket package.

--- tests/unit/lib/requestInit.js
+++ tests/unit/lib/requestInit.js
@@ -30,10 +30,14 @@ exports.requestInitTest = {
     'use strict';
 
     var requestInit  = require('../../../lib/requestInit'),
+        requestBad   = null,
+        requestNone  = { url : 'http://localhost/' },
         requestMixed = { url : 'http://localhost/?someDevice=sTrEaM' },
         requestNo    = { url : 'http://localhost/?someDevice=VolUp' },
         requestYes   = { url : 'http://localhost/?someDevice=stream' };
 
+    test.strictEqual(requestInit.requestIsStream(requestBad),   false, 'Bad commands should return false');
+    test.strictEqual(requestInit.requestIsStream(requestNone),  false, 'Unknown commands should return false');
     test.strictEqual(requestInit.requestIsStream(requestMixed), true,  'Stream commands are case insensitive');
     test.strictEqual(requestInit.requestIsStream(requestNo),    false, 'Non-stream commands should return false');
     test.strictEqual(requestInit.requestIsStream(requestYes),   true,  'Stream commands should return true');
